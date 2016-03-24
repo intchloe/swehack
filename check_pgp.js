e498f025e3440529936df9efe0ec0ebb91406bc4
intchloe <chloe@cmail.nu>
2016-03-24 19:34:31 +0100

    Create check_pgp.js

---
+++ check_pgp.js
@@ -0,0 +1,24 @@
+/* 
+
+
+This file should be called in the posting_editor.html and have this:
+<!-- IF S_PRIVMSGS --> onclick="return submit_by_id();" <!-- ENDIF -->
+in the "Send"-button. Like this:
+
+<input type="submit" accesskey="s"  <!-- IF S_PRIVMSGS --> onclick="return submit_by_id();" <!-- ENDIF --> tabindex="6" name="post" value="{L_SUBMIT}" class="button1 default-submit-action" />
+
+
+*/
+
+
+function submit_by_id() { 
+
+var t = document.getElementById('message').value; 
+if (!t.startsWith("-----BEGIN PGP MESSAGE-----")) { 
+  var r = confirm("Du skickar detta meddelande i klartext! Kolla på mottagarens profil om du kan hitta medlemmens Keybase och kryptera meddelandet med deras nyckel. Vill du skicka det ändå?"); 
+  if (r == true) { 
+  	return true
+  }
+  return false;
+  }
+}
